6d3bf160c7d26f12492ed80cafd953a0f2979cd5
Gabriel Glatz <gabrielglatz13@gmail.com>
2021-01-27 17:58:33 -0300

    adicionando maps api

--- public/js/script.js
+++ public/js/script.js
@@ -8,3 +8,41 @@ function menu_mobile() {
         list.style.transitionDuration = "0s"
     }
 }
+
+window.onload = function() {
+  let map;
+  let LatLng = {
+    'lat': -22.895018,
+    'lng': -45.3800357
+  };
+
+  function initialize () {
+    let mapProp = {
+      center: new google.maps.LatLng(LatLng),
+      scrollWheel: false,
+      zoom: 17,
+      mapTypeId: 'roadmap'
+    }
+
+    map = new google.maps.Map(document.getElementById("map"), mapProp);
+  }
+
+  function addMarker(icon) {
+    let marker = new google.maps.Marker({
+      position: LatLng,
+      map: map,
+      icon: icon
+    });
+
+    let infoWindow = new google.maps.InfoWindow({
+      content: content,
+      maxWidth: 200,
+      pixelOffset: new google.mps.Size(0, 20)
+    });
+
+    infoWindow.open(map, Marker);
+  }
+
+  initialize();
+  addMarker('');
+}
